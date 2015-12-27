9bb59fa7933784c9c08d960df228ba31d5131a51
Daniel Casper <dcasper@nectarom.com>
2015-12-27 13:56:51 -0600

    separate winner and voting components.

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,9 @@ import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+require('./style.css');
+
 ReactDOM.render(
-  <Voting pair={pair} />,
-  document.getElementById('app')
+    <Voting pair={pair} winner="Trainspotting"/>,
+    document.getElementById('app')
 );
